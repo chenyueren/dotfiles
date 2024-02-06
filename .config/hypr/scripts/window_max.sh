cb75b252ceb624437509aa33f7d1f5ddb80b1893
chenyueren <chen.yaojen@gmail.com>
2024-02-06 14:54:14 +0800

    fix scripts

--- .config/hypr/scripts/window_max.sh
+++ .config/hypr/scripts/window_max.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-MONITOR_WIDTH_HEIGHT_SCALE=$(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | (.width|tostring) + " " + (.height|tostring) + " " + (.scale|floor|tostring)')
+# 取得螢幕的寬、高、與縮放比
+# 縮放比可能為小數，為避免浮點計算問題，先乘10，之後再除回來
+MONITOR_WIDTH_HEIGHT_SCALE=$(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | (.width|tostring) + " " + (.height|tostring) + " " + (.scale*10|floor|tostring)')
 read MONITOR_WIDTH MONITOR_HEIGHT SCALE <<< $MONITOR_WIDTH_HEIGHT_SCALE
+
+# 取得狀態列高度
 STATUSBAR_HEIGHT=$(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | .reserved[1]')
+
+# 取得視窗間隙
 HYPRLAND_GAPS_OUT=$(hyprctl getoptions general:gaps_out -j | jq -r '.int')
+# 取得視窗邊框大小
 HYPRLAND_BORDER_SIZE=$(hyprctl getoptions general:border_size -j | jq -r '.int')
-AVAILABLE_WIDTH=$((MONITOR_WIDTH/SCALE - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
-AVAILABLE_HEIGHT=$((MONITOR_HEIGHT/SCALE - STATUSBAR_HEIGHT - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
 
+# 計算視窗最大化可用寬度
+AVAILABLE_WIDTH=$((MONITOR_WIDTH*10/SCALE - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
+# 計算視窗最大化可用高度
+AVAILABLE_HEIGHT=$((MONITOR_HEIGHT*10/SCALE - STATUSBAR_HEIGHT - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
 
+# 取得目前視窗是否浮動中
 floating=$(hyprctl activewindow -j | jq '.floating')
+# 視窗非浮動時，切成浮動狀態，並放到最大置中
+# 視窗浮動時，切回非浮動（平鋪）狀態
 if [[ $floating == 'false' ]];then
     hyprctl dispatch togglefloating
     hyprctl dispatch resizeactive exact $AVAILABLE_WIDTH $AVAILABLE_HEIGHT
--- .config/hypr/scripts/window_next.sh
+++ .config/hypr/scripts/window_next.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MONITOR_WIDTH_HEIGHT_SCALE=$(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | (.width|tostring) + " " + (.height|tostring) + " " + (.scale|floor|tostring)')
-read MONITOR_WIDTH MONITOR_HEIGHT_SCALE <<< $MONITOR_WIDTH_HEIGHT_SCALE
+MONITOR_WIDTH_HEIGHT_SCALE=$(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | (.width|tostring) + " " + (.height|tostring) + " " + (.scale*10|floor|tostring)')
+read MONITOR_WIDTH MONITOR_HEIGHT SCALE <<< $MONITOR_WIDTH_HEIGHT_SCALE
 STATUSBAR_HEIGHT=$(hyprctl monitors -j | jq -r 'map(select(.focused == true)) | .[] | .reserved[1]')
 HYPRLAND_GAPS_OUT=$(hyprctl getoptions general:gaps_out -j | jq -r '.int')
 HYPRLAND_BORDER_SIZE=$(hyprctl getoptions general:border_size -j | jq -r '.int')
-AVAILABLE_WIDTH=$((MONITOR_WIDTH/SCALE - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
-AVAILABLE_HEIGHT=$((MONITOR_HEIGHT/SCALE - STATUSBAR_HEIGHT - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
+AVAILABLE_WIDTH=$((MONITOR_WIDTH*10/SCALE - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
+AVAILABLE_HEIGHT=$((MONITOR_HEIGHT*10/SCALE - STATUSBAR_HEIGHT - HYPRLAND_GAPS_OUT*2 - HYPRLAND_BORDER_SIZE*2))
 
 
 floating=$(hyprctl activewindow -j | jq '.floating')
